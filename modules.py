2b4f644804ee506b427be47e0b9a48e96084c979
gremy14 <205607@pennmanor.net>
2026-03-09 10:26:29 -0400

    Update and rename main.py to modules.py

---
+++ modules.py
@@ -0,0 +1,47 @@
+import math
+#imports the math modules, give functions like logs and some numbers like pi and e(exponential growth)
+
+import os
+#Allows computer to connnect dirrectly to the systems os
+#Adds, getting current folder, listing files, making a new folder, and deleting files 
+
+import sys
+#Allows your computer to interact with the python on your runtime enviorment
+#Command Line Arguments (sys.argv) It captures the arguments you pass to a script when you run it from the terminal
+#Interacting with the Interpreter 
+#(sys.exit)  -
+#(sys.path)  -
+#(sys.version)-
+
+
+import pathlib
+
+
+import json
+
+
+import re
+
+
+import csv
+
+
+import requests
+
+
+import unittest
+
+
+import Django
+
+
+import TensorFlow
+
+
+import Pandas
+
+
+import datatime
+
+
+import logging
